905ef01067bf43adba4d87c1603c90a40afdb46c
Alan Cruz <alan_mugiwara@hotmail.com>
2024-02-05 19:33:08 -0300

    Remocao das aspas nas linhas 38, 41 | alan-home script

---
+++ 7z-mugi-sliptter-1980mb-only-slipt.bat
@@ -0,0 +1,50 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Defina o caminho para o executável 7-Zip
+set "sevenZip=C:\Program Files\7-Zip\7z.exe"
+
+REM Verifique se foram fornecidos argumentos
+if "%~1"=="" (
+    echo Uso: %0 arquivo_ou_pasta_1 [arquivo_ou_pasta_2 ...]
+    exit /b 1
+)
+
+REM Inicialize a mensagem de conclusão
+set "conclusion_message="
+
+REM Percorra todos os argumentos
+for %%A in (%*) do (
+    REM Verifique se o arquivo ou pasta fornecido existe
+    if not exist "%%~A" (
+        echo O arquivo ou pasta %%~A nao foi encontrado.
+        exit /b 1
+    )
+
+    REM Nome do arquivo ou pasta de entrada sem extensão
+    if exist "%%~A\" (
+        set "baseName=%%~nA"
+    ) else (
+        set "baseName=%%~nA"
+    )
+
+    REM Compactar o arquivo ou pasta em partes de 1980MB
+    "%sevenZip%" a -t7z -mx=9 -m0=copy -mmt=8 -v1980m "!baseName!.7z" "%%~A
+
+    REM Verificar se a operação foi bem-sucedida
+    if !errorlevel! equ 0 (
+        set "conclusion_message=!conclusion_message!Tarefa finlizada: %%~A^
+
+    ) else (
+        set "conclusion_message=!conclusion_message!Erro durante a divisao do arquivo: %%~A^
+
+    )
+)
+
+REM Exibir mensagem de conclusão
+echo.
+echo A.L.E.R.T.A:
+echo !conclusion_message!
+
+pause
+endlocal
